c3db2fb4767497f8a3e9ee8849147b7b56863303
Sean Miller <smiller206@me.com>
2018-03-30 11:26:28 -0700

    finshed comments

---
+++ sean-joanna/client/scripts/views/admin-view.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var app = app || {};
+
+const ENV = {};
+
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'insert cloud API server URL here';
+ENV.developmentApiUrl = 'insert local API server URL here';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+(function (module) {
+  const adminView = {};
+
+  adminView.initAdminPage = function (ctx, next) {
+    $('.nav-menu').slideUp(350);
+    $('.admin-view').show();
+
+    $('#admin-form').on('submit', function(event) {
+      event.preventDefault();
+      let token = event.target.passphrase.value;
+
+      // COMMENT: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      //The token is not put into local storage in this get method. It does an API call for the token and then sets the state of localstorage.token to true if the token exists in the enviroment settings. The verify method checks if the localstorage has the token, otherwise the program adds a new class of admin-only to all elements with admin class. This makes all elements of admin un accessable to the user without the token verification.
+      $.get(`${ENV.apiUrl}/api/v1/admin`, {token})
+        .then(res => {
+          localStorage.token = true;
+          page('/');
+        })
+        .catch(() => page('/'));
+    })
+  };
+
+  adminView.verify = function(ctx, next) {
+    if(!localStorage.token) $('.admin').addClass('admin-only');
+    else $('.admin').show();
+    next();
+  }
+
+  module.adminView = adminView;
+})(app)
